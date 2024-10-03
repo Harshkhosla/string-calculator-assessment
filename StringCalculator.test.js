3599094300239b221298309c383557659d31b583
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 15:56:14 +0530

    added the test for the non empty strings and created a helper function for the test

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,9 +1,18 @@
 const StringCalculator = require('./StringCalculator');
 
+const checkResult = (expression, result) => {
+    it(`should evaluate "${expression}" to ${result}`, () => {
+      expect(StringCalculator.add(expression)).toBe(result);
+    });
+  };
+
 describe('String Calculator', () => {
   
     it('should evaluate empty string to 0', () => {
       expect(StringCalculator.add("")).toBe(0);
     });
 
+    describe('comma-separator',()=>{
+        checkResult("1,2",3);
+    })
 })

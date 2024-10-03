6368ad6cf9575a9513d0f16c494a25dd9e12592f
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 15:58:10 +0530

    Created the logic for the non empty string

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,7 +2,13 @@ const StringCalculator = {
     add: (expression) => {
       if (expression === "") return 0;
   
-     
+       const pieces= expression.split(',');
+       let sum = 0;
+       
+       for(let i = 0 ;i<pieces.length;i++){
+        sum += parseInt(pieces[i]) || 0;
+       }
+       return sum ;
     }
   };
   

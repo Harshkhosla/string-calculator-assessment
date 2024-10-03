64312e5bd7d3902e811ba842f8aecc7e6bc96ff6
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 18:27:51 +0530

    added the logic for the sum greater than 1000

--- StringCalculator.js
+++ StringCalculator.js
@@ -40,12 +40,16 @@ const StringCalculator = {
     },
 
     calculateSum: function (pieces) {
-        let sum = 0;
-        for (let i = 0; i < pieces.length; i++) {
-            sum += parseInt(pieces[i]) || 0;
+        var sum = 0;
+    
+        for (var i = 0; i < pieces.length; i++) {
+          if (parseInt(pieces[i]) <= 1000) { 
+            sum += parseInt(pieces[i]) || 0; 
+          }
         }
+    
         return sum;
-    },
+      },
 
     hasCustomDelimiter: function (expression) {
         return expression.startsWith("//");

25c77cafa2f28073421f0b5e47bfb0880edf8994
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 16:47:49 +0530

    adding the code structing for reusablity and core re factoring

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,11 +1,27 @@
 const StringCalculator = {
     add: function (expression) {
-        if (expression === "") return 0;
-
-        const pieces = expression.split(/,|\n/g);
+          const pieces = this.getPieces(expression);
         return this.calculateSum(pieces);
     },
+    getPieces: function (expression) {
+      let delimiters = [",", "\n"];  
 
+      return this.getSubPieces([expression], delimiters); 
+    },
+    getSubPieces: function (piecesSoFar, delimiters) {
+      if (delimiters.length === 0) {
+        return piecesSoFar;
+      }
+  
+      const subPieces = [];
+      const delimiter = delimiters.pop();  
+      
+      for (let i = 0; i < piecesSoFar.length; i++) {
+        subPieces.push(...piecesSoFar[i].split(delimiter));
+      }
+      return this.getSubPieces(subPieces, delimiters);
+    },
+    
     calculateSum: function (pieces) {
         let sum = 0;
         for (let i = 0; i < pieces.length; i++) {
@@ -13,7 +29,7 @@ const StringCalculator = {
         }
         return sum;
     },
-
+    
 };
 
 module.exports = StringCalculator;

99a7289aa963041cb7b6a49ac7697f15cea14429
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 19:54:09 +0530

    changing the code and refactoring it

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,87 +1,84 @@
 const StringCalculator = {
     add: function (expression) {
-          const pieces = this.getPieces(expression);
-          this.checkValidity(pieces);
+        const pieces = this.getPieces(expression);
+        this.checkValidity(pieces);
         return this.calculateSum(pieces);
     },
-    checkValidity: function (pieces) {
-        var negatives = [];
-    
-        for (var i = 0; i < pieces.length; i++) {
-          if (parseInt(pieces[i]) < 0) {
-            negatives.push(pieces[i]); 
-          }
-        }    
-        if (negatives.length) {
-          throw "Negative numbers are not allowed: " + negatives.join(", ");
-        }
-    },
     getPieces: function (expression) {
-      let delimiters = [",", "\n"];  
-      if (this.hasCustomDelimiter(expression)) {
-        delimiters = delimiters.concat(this.getCustomDelimiters(expression));
-        expression = this.stripFirstLine(expression);
-      }
-    //   return this.getSubPieces([expression], delimiters); 
-    var pieces = this.getSubPieces([expression], delimiters); 
-    var pieceValues = [];
-    for (var i = 0; i < pieces.length; i++) {
-        if (pieces[i] !== "") {
-            pieceValues.push(parseInt(pieces[i]));
-          }
-    }
-
-    return pieceValues;
+        let delimiters = [",", "\n"];
+        if (this.hasCustomDelimiter(expression)) {
+            delimiters = delimiters.concat(this.getCustomDelimiters(expression));
+            expression = this.stripFirstLine(expression);
+        }
+        //   return this.getSubPieces([expression], delimiters); 
+        var pieces = this.getSubPieces([expression], delimiters);
+        var pieceValues = [];
+        for (var i = 0; i < pieces.length; i++) {
+            if (pieces[i] !== "") {
+                pieceValues.push(parseInt(pieces[i]));
+            }
+        }
+        return pieceValues;
     },
+
+    hasCustomDelimiter: function (expression) {
+        return expression.startsWith("//");
+    },
+
     getCustomDelimiters: function (expression) {
         var delimiters = [];
         var delimiterPart = expression.split("\n")[0];
         var matches = delimiterPart.match(/\[(.*?)\]/g);
-        
         if (matches) {
-          matches.forEach(match => {
-            delimiters.push(match.slice(1, -1)); 
-          });
+            matches.forEach(match => {
+                delimiters.push(match.slice(1, -1));
+            });
         } else {
-          delimiters.push(delimiterPart.slice(2));
+            delimiters.push(delimiterPart.slice(2));
         }
-    
         return delimiters;
-      },
-
-      stripFirstLine: function (expression) {
-        return expression.split("\n").slice(1).join("\n"); 
-      },
+    },
 
     getSubPieces: function (piecesSoFar, delimiters) {
-      if (delimiters.length === 0) {
-        return piecesSoFar;
-      }
-  
-      const subPieces = [];
-      const delimiter = delimiters.pop();  
-      
-      for (let i = 0; i < piecesSoFar.length; i++) {
-        subPieces.push(...piecesSoFar[i].split(delimiter));
-      }
-      return this.getSubPieces(subPieces, delimiters);
+        if (delimiters.length === 0) {
+            return piecesSoFar;
+        }
+        const subPieces = [];
+        const delimiter = delimiters.pop();
+
+        for (let i = 0; i < piecesSoFar.length; i++) {
+            subPieces.push(...piecesSoFar[i].split(delimiter));
+        }
+        return this.getSubPieces(subPieces, delimiters);
+    },
+
+    stripFirstLine: function (expression) {
+        return expression.split("\n").slice(1).join("\n");
+    },
+
+    checkValidity: function (pieces) {
+        var negatives = [];
+
+        for (var i = 0; i < pieces.length; i++) {
+            if (parseInt(pieces[i]) < 0) {
+                negatives.push(pieces[i]);
+            }
+        }
+        if (negatives.length) {
+            throw "Negative numbers are not allowed: " + negatives.join(", ");
+        }
     },
 
     calculateSum: function (pieces) {
         var sum = 0;
-    
+
         for (var i = 0; i < pieces.length; i++) {
-          if (parseInt(pieces[i]) <= 1000) { 
-            sum += parseInt(pieces[i]) || 0; 
-          }
+            if (parseInt(pieces[i]) <= 1000) {
+                sum += parseInt(pieces[i]) || 0;
+            }
         }
         return sum;
-      },
-
-    hasCustomDelimiter: function (expression) {
-        return expression.startsWith("//");
-      }
-      
+    },
 };
 
 module.exports = StringCalculator;

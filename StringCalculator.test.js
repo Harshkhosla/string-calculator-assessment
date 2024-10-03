55d0705ccea2fcdd0ae853a97b9e968beaaff94a
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-03 16:57:12 +0530

    adding the test for the negative no and adding the exception error

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -2,32 +2,48 @@ const StringCalculator = require('./StringCalculator');
 
 const checkResult = (expression, result) => {
     it(`should evaluate "${expression.replace(/\n/g, "\\n")}" to ${result}`, () => {
-      expect(StringCalculator.add(expression)).toBe(result);
+        expect(StringCalculator.add(expression)).toBe(result);
     });
-  };
+};
 
 describe('String Calculator', () => {
-  
+
     it('should evaluate empty string to 0', () => {
-      expect(StringCalculator.add("")).toBe(0);
+        expect(StringCalculator.add("")).toBe(0);
     });
 
-    describe('comma-separator',()=>{
-        checkResult("1,2",3);
-        checkResult("7,2",9);
+    describe('comma-separator', () => {
+        checkResult("1,2", 3);
+        checkResult("7,2", 9);
     })
 
-    describe('new-line-seperator',()=>{
+    describe('new-line-seperator', () => {
         checkResult("1\n4", 5);
         checkResult("4\n2\n3\n4\n5", 18);
     })
 
-    describe('mixing-comma-inline-seperator',()=>{
+    describe('mixing-comma-inline-seperator', () => {
         checkResult("3\n2,3\n4,5", 17);
     })
 
     describe('custom separator', () => {
-        checkResult("//;\n1;2", 3);  
+        checkResult("//;\n1;2", 3);
     });
-    
+
+    describe('negative numbers are not allowed', () => {
+        var caught = null;
+
+        before(function () {
+            try {
+                StringCalculator.add("-1");
+            } catch (ex) {
+                caught = ex; 
+            }
+        });
+
+        it('should throw an exception', function () {
+            expect(caught).toBeTruthy(); 
+        });
+    });
+
 })
